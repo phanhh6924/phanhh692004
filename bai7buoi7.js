99e3965e15bad1871a5e1087089c5be9ab0406f5
phanhh69204 <nocnan0705@gmail.com>
2024-01-29 11:40:42 +0700

    first commit

---
+++ bai7buoi7.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.querySelectorAll('.submenu');
+  
+    dropdowns.forEach(function (dropdown) {
+      dropdown.addEventListener('click', function () {
+        this.classList.toggle('active');
+      });
+    });
+  });
+  
